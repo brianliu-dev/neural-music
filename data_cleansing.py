9649f5a625bde6197b6f0216c0d1f83977bde704
Liu <bliu4@MTVL1539012da.local>
2017-09-02 01:35:24 -0700

    First step - data processing

---
+++ data_cleansing.py
@@ -0,0 +1,31 @@
+import numpy as np
+import os
+
+file_path = "./data/large_dataset.txt"
+
+# Mostly pseudocode for now
+
+start = 0
+end = 107
+notes = np.array(150000)[104]
+
+# Parse, remove blemishes
+def parse_line(line):
+	# do stuff
+	return line
+	
+conversion_table = {}
+
+def convert_to_ascii(line):
+	# convert to ascii
+	return line
+
+def inverse_conversion():
+	# Need to convert back to midi format!
+	
+
+# Read in data from file
+with open(file_path) as f:
+    parse_line(f.readline())
+    count+=1
+
